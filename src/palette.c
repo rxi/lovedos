0506b3b0ca0c9d27fcdda2f0098686352a1ec7ef
rxi <rxi@users.noreply.github.com>
2016-09-30 20:55:57 +0100

    Added missing copyright header comment to palette.c/h

--- src/palette.c
+++ src/palette.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2016 rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
 #include <stdlib.h>
 #include <pc.h>
 
