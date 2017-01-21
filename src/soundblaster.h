d78f20367427466c3b4c8e3dec68ce64363b7ceb
rnlf <rnlf@users.noreply.github.com>
2017-01-21 21:45:49 +0100

    Started work on Soundblaster driver

---
+++ src/soundblaster.h
@@ -0,0 +1,20 @@
+#ifndef SOUNDBLASTER_H
+#define SOUNDBLASTER_H
+
+#include <stdint.h>
+
+// Error codes
+#define SOUNDBLASTER_ENV_NOT_SET 1
+#define SOUNDBLASTER_ENV_INVALID 2
+#define SOUNDBLASTER_DOS_ERROR   3
+#define SOUNDBLASTER_RESET_ERROR 4
+#define SOUNDBLASTER_ALLOC_ERROR 5
+
+#define SOUNDBLASTER_SAMPLES_PER_BUFFER 2048
+
+typedef int16_t const* (*soundblaster_getSampleProc)(void);
+
+int soundblaster_init(soundblaster_getSampleProc sampleproc);
+void soundblaster_deinit(void);
+
+#endif
